1d73bfe4647c1e48e3dbcb4cb336fcff11f33613
jason07289 <38865267+jason07289@users.noreply.github.com>
2018-05-03 04:28:18 +0900

    f
    f

---
+++ register.sol
@@ -0,0 +1,45 @@
+pragma solidity ^0.4.0; 
+
+ 
+
+contract Register {
+    
+    uint birth;
+    string name;
+    string imagepath;
+
+
+    function SetBirth(uint ubirth) {
+        
+        birth = ubirth;
+
+    }
+    function SetName(string uname) {
+        
+        name = uname;
+
+    }
+    function SetImagePath(string uimage) {
+        
+        imagepath = uimage;
+
+    }
+
+ 
+
+    function GetBirth() constant returns (uint) {
+
+        return birth;
+
+    }
+     function GetName() constant returns (string) {
+
+        return name;
+
+    }
+     function GetImagePath() constant returns (string) {
+
+        return imagepath;
+
+    }
+}
